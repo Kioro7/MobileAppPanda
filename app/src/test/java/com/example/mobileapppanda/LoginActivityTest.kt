a6c9fe49f40ec22c3f46390116bd1fad8dec46df
Ilya Zamyczkij <zamyczkij_i@garpix.com>
2023-11-25 16:26:34 +0300

    Рефакторинг

--- app/src/test/java/com/example/mobileapppanda/LoginActivityTest.kt
+++ app/src/test/java/com/example/mobileapppanda/LoginActivityTest.kt
@@ -7,13 +7,13 @@ class LoginActivityTest {
 
     @Test
     fun testLoginWithCorrect() {
-        val result = LoginActivity.AuthUtil.loginValidation("ilzam@mail.ru", "Aa123456!")
+        val result = AuthUtil.loginValidation("ilzam@mail.ru", "Aa123456!")
         assertEquals(true, result)
     }
 
     @Test
     fun testLoginWithIncorrect() {
-        val result = LoginActivity.AuthUtil.loginValidation("incorrect@mail.ru", "wrongpassword")
+        val result = AuthUtil.loginValidation("incorrect@mail.ru", "wrongpassword")
         assertEquals(false, result)
     }
 }
